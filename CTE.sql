b8f30cf0fa54b09ea36f3629b00e3bc89d80b2e1
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-20 20:43:09 -0300

    Update CTE.sql

--- CTE.sql
+++ CTE.sql
@@ -3,7 +3,8 @@ WITH tbl_tmp_banco AS(
 	FROM banco
 )
 SELECT numero, nome
-FROM tbl_tpm_banco;
+FROM tbl_tmp_banco;
+
 
 WITH params AS (
 	SELECT 213 AS banco_numero
@@ -12,3 +13,18 @@ WITH params AS (
 	FROM
 	JOIN params ON params.banco_numero = banco.numero
 )
+SELECT numero,nome
+FROM tbl_tmp_banco;
+
+
+WITH clientes_e_transacoes AS(
+	SELECT cliente.nome AS cliente_nome,
+		tipo_transacao.nome AS tipo_transacao_nome,
+		cliente_transacoes.valor AS tipo_transacao_valor
+	FROM cliente_transacoes
+	JOIN cliente ON cliente.numero = cliente_transacoes.cliente_numero
+	JOIN tipo_transacao ON tipo_transacao = cliente_transacoes.tipo_transacao_id
+	JOIN banco ON banco.numero = cliente_transacoes.banco_numero and banco.nome ILIKE '%Itaú%'
+)
+SELECT cliente_nome, tipo_transacao_nome, tipo_transacao_valor
+FROM clientes_e_transacoes
